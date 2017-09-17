e7adab5522086c1008ae4360343a52085bc2ae92
Jose Guerra <jose@gcostudios.com>
2017-09-17 12:22:58 +0100

    Updated js file to include a simple hamburger toggle

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-(function ($) {
-  $(function () {
-	$('nav ul li > a:not(:only-child)').click(function(e) {
-	  $(this).siblings('.nav-dropdown').slideToggle();
-	  $('.nav-dropdown').not($(this).siblings()).hide();
-	  e.stopPropagation();
-	});
-	$('html').click(function() {
-	  $('.nav-dropdown').hide();
-	});
-  });
-  document.querySelector('#nav-toggle').addEventListener('click', function() {
-	this.classList.toggle('active');
-  });
-  $('#nav-toggle').click(function() {
-	$('nav ul').slideToggle();
-  });
-})(jQuery);
+$('.hamburger').on('click', function() {
+	$(this).toggleClass('opened');
+});
