f72bf371be16618eaedac3db84004c14cf09d29b
Jose Guerra <jose@gcostudios.com>
2017-10-02 14:16:42 +0100

    Added is-noscroll class

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function ($) {
 	$('.hamburger, .btn-close').on('click', function(){
 		$(this).toggleClass('opened');
 		$('nav').toggleClass('open');
+		$('body, html').toggleClass('is-noscroll');
 	})
 
 });
